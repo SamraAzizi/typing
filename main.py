35956c181a630f31bb52caec4e30ead10f55f439
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-22 16:08:17 +0500

    my commit

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from curses import wrapper
 
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr("it is a technology time where computers are normal!")
+    stdscr.addstr("it is a technology time where computers are normal!", curses.color_pair(1))
     stdscr.refresh()
     stdscr.getKey()
 
