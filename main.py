4fe7430f21131de1f18e5356846e341be4f202dd
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-22 16:09:37 +0500

    my commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr("it is a technology time where computers are normal!", curses.color_pair(1))
+    stdscr.addstr(1, 0, "it is a technology time where computers are normal!")
     stdscr.refresh()
     stdscr.getKey()
 
