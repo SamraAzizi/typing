11dfbbb977e658f827ae520dbdaad135f07a0b28
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-23 11:35:17 +0500

    my commit

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import curses
 from curses import wrapper
 
 def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr( "Welcome To The Speed Typing Test")
+    stdscr.addstr("\nPress Any Key To Begin")
+    stdscr.refresh()
+    stdscr.getkey()
+    
     
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr(0, 0, "it is a technology time where computers are normal!")
-    stdscr.refresh()
-    stdscr.getKey()
+    
+    start_screen(stdscr)
 
 
 wrapper(main)
